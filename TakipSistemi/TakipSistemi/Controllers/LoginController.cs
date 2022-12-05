6bb9bf58d919600e99b35ca6cb66b3ac8c10de44
Burak Gün <burak.gun@hotmail.com>
2022-12-05 23:45:57 +0300

    Login Page Test
    Login page and some css examples tested

---
+++ TakipSistemi/TakipSistemi/Controllers/LoginController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TakipSistemi.Models;
+
+namespace TakipSistemi.Controllers
+{
+    public class LoginController : Controller
+    {
+        UserDbContext _context = new UserDbContext();
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+
+
+        [HttpPost]
+        public IActionResult Login(LoginData login)
+        {
+
+            if (ModelState.IsValid)
+            {
+                // Check If user is created before
+                // Return value(if data is exist) or null(if data is not available)
+                var queryResult = _context.Users.FirstOrDefault(input => input.EmailAddr == login.EmailAddr);
+
+                //
+                if (queryResult != null)
+                {
+                    TempData["message"] = "There is an account associated with this e-mail address. Control your e-mail address or try to login!";
+                    return View();
+                }
+                _context.Users.Add(login);
+                _context.SaveChanges();
+            }
+            return View();
+        }
+    }
+}
---
+++ TakipSistemi/TakipSistemi/Models/UserDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TakipSistemi.Models
+{
+    public class UserDbContext : DbContext
+    {
+        public DbSet<LoginData> Users { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //connection string : "Server=(localdb)\mssqllocaldb; Database=ECommerceDb; Trusted_Connection=True;"
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb; Database=VehicleTracking; Trusted_Connection=True;");
+        }
+    }
+}
